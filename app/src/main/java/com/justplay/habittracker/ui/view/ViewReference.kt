8d993666823f3f442bf55818e3b1c184472c6c8f
justplay0526 <justplay0526@gmail.com>
2025-12-05 16:46:39 +0800

    style: add HabitPeriodStringRes on ViewReference

--- app/src/main/java/com/justplay/habittracker/ui/view/ViewReference.kt
+++ app/src/main/java/com/justplay/habittracker/ui/view/ViewReference.kt
@@ -1,5 +1,13 @@
 package com.justplay.habittracker.ui.view
 
 import androidx.compose.ui.unit.dp
+import com.justplay.habittracker.R
 
 val HabitListItemHeight = 68.dp
+
+val HabitPeriodStringRes = listOf(
+    R.string.text_time_of_day_all,
+    R.string.text_time_of_day_morning,
+    R.string.text_time_of_day_afternoon,
+    R.string.text_time_of_day_evening,
+)
