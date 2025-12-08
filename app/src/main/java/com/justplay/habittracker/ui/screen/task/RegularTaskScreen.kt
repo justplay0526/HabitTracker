e8bad088804e0a7fd9c32292c0fe3366b8bf9791
justplay0526 <justplay0526@gmail.com>
2025-12-08 17:03:48 +0800

    style: adjust RegularTaskScreen layout
    what:
    - add 5 Outlined Icon
    - add View All Text to show ModalBottomSheet

--- app/src/main/java/com/justplay/habittracker/ui/screen/task/RegularTaskScreen.kt
+++ app/src/main/java/com/justplay/habittracker/ui/screen/task/RegularTaskScreen.kt
@@ -1,50 +1,136 @@
 package com.justplay.habittracker.ui.screen.task
 
+import android.widget.Toast
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.justplay.habittracker.R
+import com.justplay.habittracker.ui.theme.HabitTrackerTheme
 import com.justplay.habittracker.ui.view.HabitInputField
+import com.justplay.habittracker.ui.view.IconModalBottomSheet
+import com.justplay.habittracker.ui.view.IconsRes
+import com.justplay.habittracker.ui.view.OutlinedIcon
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RegularTaskScreen() {
+    val context = LocalContext.current
+    var selectedIcon by remember { mutableIntStateOf(-1) }
+
     var text by remember { mutableStateOf("") }
+    var showIconPicker by remember { mutableStateOf(false) }
+    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
 
     Column(
         modifier = Modifier.fillMaxSize()
+            .padding(horizontal = 16.dp)
     ) {
         Text(
             text = stringResource(R.string.title_habit_name),
-            style = MaterialTheme.typography.titleLarge,
-            modifier = Modifier.padding(horizontal = 16.dp)
+            style = MaterialTheme.typography.titleLarge
         )
 
-        Spacer(modifier = Modifier.height(4.dp))
+        Spacer(modifier = Modifier.height(8.dp))
 
         HabitInputField(
             value = text,
             onValueChange = { text = it },
-            placeholder = stringResource(R.string.title_habit_name),
-            modifier = Modifier.padding(horizontal = 16.dp)
+            placeholder = stringResource(R.string.title_habit_name)
         )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = stringResource(R.string.title_icon),
+                style = MaterialTheme.typography.titleLarge
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            Row(
+                modifier = Modifier
+                    .clickable { showIconPicker = true },
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "View All",
+                    color = MaterialTheme.colorScheme.primary,
+                    style = MaterialTheme.typography.titleLarge
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowForward,
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            IconsRes.take(5).forEach { item ->
+                OutlinedIcon(
+                    iconRes = item,
+                    selected = selectedIcon == item,
+                    onClick = { selectedIcon = item }
+                )
+            }
+        }
+
+        if(showIconPicker) {
+            IconModalBottomSheet(
+                sheetState = sheetState,
+                onIconSelected = { icon ->
+                    // TODO Handle icon selection
+                    Toast.makeText(context, "Selected icon: $icon", Toast.LENGTH_SHORT).show()
+                },
+                onCancel = { showIconPicker = false }
+            )
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun RegularTaskScreenPreview() {
-    RegularTaskScreen()
+    HabitTrackerTheme {
+        RegularTaskScreen()
+    }
 }
--- app/src/main/java/com/justplay/habittracker/ui/view/ViewReference.kt
+++ app/src/main/java/com/justplay/habittracker/ui/view/ViewReference.kt
@@ -5,10 +5,29 @@ import com.justplay.habittracker.R
 
 val HabitListItemHeight = 68.dp
 val HabitTextFieldHeight = 52.dp
+val IconBoxSize = 64.dp
 
 val HabitPeriodStringRes = listOf(
     R.string.text_time_of_day_all,
     R.string.text_time_of_day_morning,
     R.string.text_time_of_day_afternoon,
     R.string.text_time_of_day_evening,
+)
+
+val IconsRes = listOf(
+    R.mipmap.emoji_01_smile_sunglasses, R.mipmap.emoji_02_slightly_smile,
+    R.mipmap.emoji_03_smile_heart_eyes, R.mipmap.emoji_04_kiss_close_eyes,
+    R.mipmap.emoji_05_relieved, R.mipmap.emoji_06_smirking,
+    R.mipmap.emoji_07_savoring_food, R.mipmap.emoji_08_tears_joy,
+    R.mipmap.emoji_09_grinning_big_eyes, R.mipmap.emoji_10_weary,
+    R.mipmap.emoji_11_winking_tongue, R.mipmap.emoji_12_sleepy,
+    R.mipmap.emoji_13_tongue, R.mipmap.emoji_14_worry,
+    R.mipmap.emoji_15_disappoint, R.mipmap.emoji_16_tired,
+    R.mipmap.emoji_17_squit_tongue, R.mipmap.emoji_18_scream_tear,
+    R.mipmap.emoji_19_anxious_sweat, R.mipmap.emoji_20_blow_kiss,
+    R.mipmap.emoji_21_grimacing, R.mipmap.emoji_22_loudly_cry,
+    R.mipmap.emoji_23_exhaling, R.mipmap.emoji_24_astonished,
+    R.mipmap.emoji_25_sleeping, R.mipmap.emoji_26_cold,
+    R.mipmap.emoji_27_spiral_eyes, R.mipmap.emoji_28_symbol_on_month,
+    R.mipmap.emoji_29_exploding_head, R.mipmap.emoji_30_smile_horns,
 )
