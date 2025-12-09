165ddb69d26007d8875bf5bc34dca4b230fce494
justplay0526 <justplay0526@gmail.com>
2025-12-09 16:42:27 +0800

    feat: add ColorPicker layout
    what:
    - add LazyVerticalGrid
    - add CircleColorBox as grid item

--- app/src/main/java/com/justplay/habittracker/ui/screen/task/RegularTaskScreen.kt
+++ app/src/main/java/com/justplay/habittracker/ui/screen/task/RegularTaskScreen.kt
@@ -6,11 +6,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -32,6 +36,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.justplay.habittracker.R
 import com.justplay.habittracker.ui.theme.HabitTrackerTheme
+import com.justplay.habittracker.ui.view.CircleColorBox
+import com.justplay.habittracker.ui.view.ColorResource
 import com.justplay.habittracker.ui.view.HabitInputField
 import com.justplay.habittracker.ui.view.IconModalBottomSheet
 import com.justplay.habittracker.ui.view.IconsRes
@@ -45,7 +51,10 @@ fun RegularTaskScreen() {
 
     var text by remember { mutableStateOf("") }
     var showIconPicker by remember { mutableStateOf(false) }
-    val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+    val sheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = true
+    )
+    var selectedIndex by remember { mutableIntStateOf(0) }
 
     if(showIconPicker) {
         IconModalBottomSheet(
@@ -59,7 +68,8 @@ fun RegularTaskScreen() {
     }
 
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
             .padding(horizontal = 16.dp)
     ) {
         Text(
@@ -114,16 +124,62 @@ fun RegularTaskScreen() {
         Row(
             modifier = Modifier
                 .fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
+            horizontalArrangement =
+                Arrangement.spacedBy(8.dp)
         ) {
             IconsRes.take(5).forEach { item ->
                 OutlinedIcon(
                     iconRes = item,
                     selected = selectedIcon == item,
+                    modifier = Modifier.weight(1f),
                     onClick = { selectedIcon = item }
                 )
             }
         }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = stringResource(R.string.title_color),
+            style = MaterialTheme.typography.titleLarge
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(5),
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalArrangement =
+                Arrangement.spacedBy(8.dp),
+            horizontalArrangement =
+                Arrangement.spacedBy(8.dp)
+        ) {
+            /**
+             * TODO handle color picker for last Circle Box
+             * TODO connect to ViewModel
+             */
+            itemsIndexed(ColorResource) {
+                index, colorData ->
+                CircleColorBox(
+                    color = colorData,
+                    selected = selectedIndex == index,
+                    onClick = {
+                        selectedIndex = index
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(1f)
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = stringResource(R.string.title_when),
+            style = MaterialTheme.typography.titleLarge
+        )
     }
 }
 
