48442d49fc5a9f33b684ed492d35a1e33eb6e0c5
justplay0526 <justplay0526@gmail.com>
2025-12-09 13:39:26 +0800

    style: move MainNavSuite enum Dest to EnumClassCollection

--- app/src/main/java/com/justplay/habittracker/data/EnumClassCollection.kt
+++ app/src/main/java/com/justplay/habittracker/data/EnumClassCollection.kt
@@ -1,5 +1,24 @@
 package com.justplay.habittracker.data
 
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.justplay.habittracker.R
+
+enum class MainNavSuiteDest(
+    @param:StringRes val title: Int,
+    @param:DrawableRes val icon: Int
+) {
+    HOME(R.string.title_home, R.drawable.round_home_24),
+    MOOD_STAT(R.string.title_mood_stat, R.drawable.round_mood_24),
+    REPORT(R.string.title_report, R.drawable.round_show_chart_24),
+    MY_HABITS(R.string.title_my_habits, R.drawable.round_grid_view_24)
+}
+
+enum class HomeNavDest(
+) {
+    CREATE_NEW_HABIT
+}
+
 enum class DragToActionValue {
     Settle,
     COMPLETE,
