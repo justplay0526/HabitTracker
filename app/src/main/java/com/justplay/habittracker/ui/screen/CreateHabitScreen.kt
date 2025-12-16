430681c554858dc8a71d36c2c73f4a4b63891e86
justplay0526 <justplay0526@gmail.com>
2025-12-16 14:07:52 +0800

    feat: add Save Button on CreateHabitScreen

--- app/src/main/java/com/justplay/habittracker/ui/screen/CreateHabitScreen.kt
+++ app/src/main/java/com/justplay/habittracker/ui/screen/CreateHabitScreen.kt
@@ -1,9 +1,11 @@
 package com.justplay.habittracker.ui.screen
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -13,7 +15,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import androidx.compose.ui.unit.dp
 import com.justplay.habittracker.R
 import com.justplay.habittracker.ui.screen.task.OneTimeTaskScreen
 import com.justplay.habittracker.ui.screen.task.RegularTaskScreen
@@ -24,19 +28,35 @@ import com.justplay.habittracker.ui.view.CustomHorizontalPager
 fun CreateNewHabitScreen(
     onBackClick: () -> Unit
 ) {
-    Scaffold(modifier = Modifier.fillMaxSize(), topBar = {
-        CenterAlignedTopAppBar(
-            title = { Text(stringResource(R.string.title_create_new_habit)) },
-            navigationIcon = {
-                IconButton(onClick = onBackClick) {
-                    Icon(
-                        imageVector = Icons.Rounded.Close,
-                        contentDescription = stringResource(R.string.app_name)
-                    )
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(stringResource(R.string.title_create_new_habit)) },
+                navigationIcon = {
+                    IconButton(onClick = onBackClick) {
+                        Icon(
+                            imageVector = Icons.Rounded.Close,
+                            contentDescription = stringResource(R.string.app_name)
+                        )
+                    }
                 }
+            )
+    },
+        bottomBar = {
+            Button(
+                onClick = onBackClick,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(all = 16.dp)
+            ) {
+                Text(
+                    text = stringResource(R.string.btn_save),
+                    fontWeight = FontWeight.SemiBold
+                )
             }
-        )
-    }) { innerPadding ->
+        }
+    ) { innerPadding ->
         val taskTabs = listOf(
             stringResource(R.string.text_habit_task_regular),
             stringResource(R.string.text_habit_task_one_time)
