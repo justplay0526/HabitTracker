4ecba9db664c04ba6c1ba0999d1e8d4aa08bf21e
justplay0526 <justplay0526@gmail.com>
2025-12-26 16:45:51 +0800

    feat: add DataDb on data module and Constant to save table name

---
+++ data/src/main/java/com/justplay/data/db/DataDb.kt
@@ -0,0 +1,37 @@
+package com.justplay.data.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.justplay.data.db.converters.DateTimeConverters
+import com.justplay.data.db.converters.EnumConverters
+import com.justplay.data.db.converters.IntSetConverters
+import com.justplay.data.db.dao.TaskDao
+import com.justplay.data.db.dao.TaskLogDao
+import com.justplay.data.db.entity.TaskEntity
+import com.justplay.data.db.entity.TaskLogEntity
+
+@Database(
+    entities = [
+        TaskEntity::class,
+        TaskLogEntity::class
+        ],
+    version = DataDb.DB_VERSION,
+    exportSchema = false
+)
+@TypeConverters(
+    DateTimeConverters::class,
+    EnumConverters::class,
+    IntSetConverters::class
+)
+abstract class DataDb : RoomDatabase() {
+    companion object {
+        /**
+         * 資料庫版本號
+         */
+        const val DB_VERSION = 1
+    }
+
+    abstract fun taskDao(): TaskDao
+    abstract fun taskLogDao(): TaskLogDao
+}
