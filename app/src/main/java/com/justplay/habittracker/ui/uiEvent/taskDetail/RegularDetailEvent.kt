398de49307c7572d1b277294f6412d40ed853bfb
justplay0526 <justplay0526@gmail.com>
2026-02-02 15:42:12 +0800

    feat: add loadLogInRange on RegularDetailEvent

--- app/src/main/java/com/justplay/habittracker/ui/uiEvent/taskDetail/RegularDetailEvent.kt
+++ app/src/main/java/com/justplay/habittracker/ui/uiEvent/taskDetail/RegularDetailEvent.kt
@@ -1,8 +1,13 @@
 package com.justplay.habittracker.ui.uiEvent.taskDetail
 
+import java.time.LocalDate
+
 sealed interface RegularDetailEvent {
     data class DeleteAndKeepHistory(val taskId: Long) : RegularDetailEvent
     data class DeleteAndClearHistory(val taskId: Long) : RegularDetailEvent
+    data class LoadLogInRange(val taskId: Long,
+                              val startDate: LocalDate,
+                              val endDate: LocalDate) : RegularDetailEvent
     data object ShowDeleteHabit : RegularDetailEvent
     data object HideDeleteHabit : RegularDetailEvent
 }
