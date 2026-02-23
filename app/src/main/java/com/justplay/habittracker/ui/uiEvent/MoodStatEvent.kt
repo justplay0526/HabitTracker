3de5c8be3988da1b14eb97ba13c191f04aa03487
justplay0526 <justplay0526@gmail.com>
2026-02-23 10:18:33 +0800

    feat: add Event on MoodStatEvent
    what:
    - added FeelingChanged
    - added MoodChanged

--- app/src/main/java/com/justplay/habittracker/ui/uiEvent/MoodStatEvent.kt
+++ app/src/main/java/com/justplay/habittracker/ui/uiEvent/MoodStatEvent.kt
@@ -1,6 +1,11 @@
 package com.justplay.habittracker.ui.uiEvent
 
+import com.justplay.habittracker.data.FeelingValue
+import com.justplay.habittracker.data.MoodValue
+
 sealed interface MoodStatEvent {
+    data class FeelingChanged(val feelingValue: FeelingValue) : MoodStatEvent
+    data class MoodChanged(val moodValue: MoodValue) : MoodStatEvent
     data object HideAddMood : MoodStatEvent
     data object ShowAddMood : MoodStatEvent
 }
