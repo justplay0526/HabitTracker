fb044b66df39563c79acc333736d77b83da3a58e
justplay0526 <justplay0526@gmail.com>
2026-02-23 10:17:25 +0800

    feat: add value on MoodStatUiState
    - added moodValue
    - added feelingValue

--- app/src/main/java/com/justplay/habittracker/ui/uiState/MoodStatUiState.kt
+++ app/src/main/java/com/justplay/habittracker/ui/uiState/MoodStatUiState.kt
@@ -1,6 +1,11 @@
 package com.justplay.habittracker.ui.uiState
 
+import com.justplay.habittracker.data.FeelingValue
+import com.justplay.habittracker.data.MoodValue
+
 data class MoodStatUiState(
-    // TODO Add Data on MoodStatUiState
+    val moodValue: MoodValue? = null,
+    val feelingValue: FeelingValue? = null,
+
     val showAddMood: Boolean = false
 )
