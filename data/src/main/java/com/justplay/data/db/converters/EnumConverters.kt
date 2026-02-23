c41c3f17bd14aa0f550fda6e9c5421bec29e0a24
justplay0526 <justplay0526@gmail.com>
2026-02-23 17:08:36 +0800

    feat: add Table Name & TypeConverters
    - added MOOD_LOG_TABLE
    - added feeling TypeConverter
    - added mood TypeConverter

--- data/src/main/java/com/justplay/data/db/converters/EnumConverters.kt
+++ data/src/main/java/com/justplay/data/db/converters/EnumConverters.kt
@@ -1,12 +1,19 @@
 package com.justplay.data.db.converters
 
 import androidx.room.TypeConverter
+import com.justplay.data.db.classPkg.FeelingValue
+import com.justplay.data.db.classPkg.MoodValue
 import com.justplay.data.db.classPkg.PeriodOption
 import com.justplay.data.db.classPkg.RepeatOption
 import com.justplay.data.db.classPkg.TaskStatus
 import com.justplay.data.db.classPkg.TaskType
 
 class EnumConverters {
+    @TypeConverter
+    fun feelingToString(value: FeelingValue?): String? = value?.name
+    @TypeConverter
+    fun stringToFeeling(value: String?): FeelingValue? = value?.let(FeelingValue::valueOf)
+
     @TypeConverter
     fun taskTypeToString(type: TaskType?): String? = type?.name
     @TypeConverter
@@ -16,6 +23,11 @@ class EnumConverters {
     fun habitPeriodToString(period: PeriodOption?): String? = period?.name
     @TypeConverter
     fun stringToHabitPeriod(value: String?): PeriodOption? = value?.let(PeriodOption::valueOf)
+
+    @TypeConverter
+    fun moodToString(value: MoodValue?): String? = value?.name
+    @TypeConverter
+    fun stringToMood(value: String?): MoodValue? = value?.let(MoodValue::valueOf)
 
     @TypeConverter
     fun statusToString(status: TaskStatus?): String? = status?.name
