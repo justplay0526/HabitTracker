21febf02f447f1f9b99fc9c78857b8092782bab9
justplay0526 <justplay0526@gmail.com>
2026-03-02 11:44:38 +0800

    feat: add MonthChanged on MonthStatEvent

--- app/src/main/java/com/justplay/habittracker/ui/uiEvent/MoodStatEvent.kt
+++ app/src/main/java/com/justplay/habittracker/ui/uiEvent/MoodStatEvent.kt
@@ -3,8 +3,10 @@ package com.justplay.habittracker.ui.uiEvent
 import com.justplay.data.db.classPkg.FeelingValue
 import com.justplay.data.db.classPkg.MoodValue
 import java.time.LocalDate
+import java.time.YearMonth
 
 sealed interface MoodStatEvent {
+    data class MonthChanged(val month: YearMonth) : MoodStatEvent
     data class MoodChanged(
         val date: LocalDate,
         val moodValue: MoodValue,
