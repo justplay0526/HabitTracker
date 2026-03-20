ec19aed60ce7126ecfdc465361c499f5aaf420f4
justplay0526 <justplay0526@gmail.com>
2026-03-20 14:46:38 +0800

    feat: added firebase implementation
    what:
    - added google service plugin on preject gradle
    - added google service plugin on app module gradle
    - added firebase bom on app module gradle
    - added firebase analytics on app module gradle
    - adjusted app gitignore content

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.services)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
@@ -52,8 +53,10 @@ android {
 
 dependencies {
     implementation(platform(libs.androidx.compose.bom))
+    implementation(platform(libs.firebase.bom))
     implementation(libs.bundles.androidx)
     implementation(libs.bundles.third.party)
+    implementation(libs.firebase.analytics)
     implementation(libs.timber)
     implementation(libs.hilt.android)
     implementation(libs.hilt.navigation.compose)
