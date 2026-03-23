a8f5b269e353275738a124d028945a4e4a938c70
justplay0526 <justplay0526@gmail.com>
2026-03-23 14:16:47 +0800

    rafactor: adjusted UiState initial value

--- app/src/main/java/com/justplay/habittracker/ui/uiState/report/ReportUiState.kt
+++ app/src/main/java/com/justplay/habittracker/ui/uiState/report/ReportUiState.kt
@@ -5,8 +5,8 @@ import com.justplay.habittracker.ui.view.customChart.MoodPoint
 
 data class ReportUiState(
     val habitCompletedRate: List<Int> = listOf(50, 60, 50, 60, 50, 70, 40),
-    val rateMax: Double = 100.toDouble(),
-    val rateMin: Double = 0.toDouble(),
+    val rateMax: Double = habitCompletedRate.max().plus(10).toDouble(),
+    val rateMin: Double = habitCompletedRate.min().minus(10).toDouble(),
     val habitCompletedCounts: List<Float> = listOf(5f, 6f, 5f, 6f, 5f, 7f, 4f),
     val habitCompletedLabels: List<String> = listOf("1", "2", "3", "4", "5", "6", "7"),
     val moodPoints: List<MoodPoint> = listOf(
