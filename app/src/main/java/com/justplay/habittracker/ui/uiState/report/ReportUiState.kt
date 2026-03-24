a22110b8f76e2579e4f59ae18319e6e2dc106a34
justplay0526 <justplay0526@gmail.com>
2026-03-24 13:06:17 +0800

    rafactor: adjust UiState Default
    what:
    - adjusted ReportUiState Default value

--- app/src/main/java/com/justplay/habittracker/ui/uiState/report/ReportUiState.kt
+++ app/src/main/java/com/justplay/habittracker/ui/uiState/report/ReportUiState.kt
@@ -1,25 +1,20 @@
 package com.justplay.habittracker.ui.uiState.report
 
-import com.justplay.data.db.classPkg.MoodValue
+import com.justplay.habittracker.completeCountsDefault
+import com.justplay.habittracker.completeLabelsDefault
+import com.justplay.habittracker.data.DailyCompleteRate
+import com.justplay.habittracker.completeRateForLineDefault
+import com.justplay.habittracker.moodPointDefault
 import com.justplay.habittracker.ui.view.customChart.MoodPoint
 import java.time.YearMonth
 
 data class ReportUiState(
-    val habitCompletedRate: List<Int> = listOf(50, 60, 50, 60, 50, 70, 40),
-    val rateMax: Double = habitCompletedRate.max().plus(10).toDouble(),
-    val rateMin: Double = habitCompletedRate.min().minus(10).toDouble(),
-    val habitCompletedCounts: List<Float> = listOf(5f, 6f, 5f, 6f, 5f, 7f, 4f),
-    val habitCompletedLabels: List<String> = listOf("1", "2", "3", "4", "5", "6", "7"),
-    val moodPoints: List<MoodPoint> = listOf(
-        MoodPoint("15", MoodValue.GREAT.ordinal),
-        MoodPoint("16", MoodValue.GOOD.ordinal),
-        MoodPoint("17", MoodValue.OKAY.ordinal),
-        MoodPoint("18", MoodValue.GREAT.ordinal),
-        MoodPoint("19", MoodValue.GREAT.ordinal),
-        MoodPoint("20", MoodValue.GOOD.ordinal),
-        MoodPoint("21", MoodValue.OKAY.ordinal),
-        MoodPoint("22", MoodValue.GREAT.ordinal),
-        MoodPoint("23", MoodValue.GREAT.ordinal),
-    )
     val currentMonth: YearMonth = YearMonth.now(),
+    val completedRateForLine: List<Int> = completeRateForLineDefault,
+    val completedRateForCalendar: List<DailyCompleteRate> = emptyList(),
+    val completeRateMax: Double = completedRateForLine.max().plus(10).toDouble(),
+    val completeRateMin: Double = completedRateForLine.min().minus(10).toDouble(),
+    val completedCounts: List<Float> = completeCountsDefault,
+    val completedLabels: List<String> = completeLabelsDefault,
+    val moodPoints: List<MoodPoint> = moodPointDefault
 )
