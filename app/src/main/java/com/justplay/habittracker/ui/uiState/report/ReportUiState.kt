8d33c0accc4885252100fd59b481eef897d9c4e2
justplay0526 <justplay0526@gmail.com>
2026-04-23 14:04:15 +0800

    refactor: refactored UiState
    what:
    - refactored ReportUiState to four sub UiState

--- app/src/main/java/com/justplay/habittracker/ui/uiState/report/ReportUiState.kt
+++ app/src/main/java/com/justplay/habittracker/ui/uiState/report/ReportUiState.kt
@@ -1,24 +1,12 @@
 package com.justplay.habittracker.ui.uiState.report
 
-import com.justplay.habittracker.completeCountsDefault
-import com.justplay.habittracker.completeLabelsDefault
-import com.justplay.habittracker.data.DailyCompleteRate
-import com.justplay.habittracker.completeRateForLineDefault
 import com.justplay.habittracker.moodPointDefault
 import com.justplay.habittracker.ui.view.customChart.MoodPoint
-import java.time.YearMonth
 
 data class ReportUiState(
-    val allCompletedCount: Int = 0,
-    val completeRate: Int = 0,
-    val completedRateForLine: List<Int> = completeRateForLineDefault,
-    val completedRateForCalendar: List<DailyCompleteRate> = emptyList(),
-    val completeRateMax: Double = completedRateForLine.max().plus(10).toDouble(),
-    val completeRateMin: Double = completedRateForLine.min().minus(10).toDouble(),
-    val completedCounts: List<Float> = completeCountsDefault,
-    val completedLabels: List<String> = completeLabelsDefault,
-    val currentMonth: YearMonth = YearMonth.now(),
-    val maxStreak: Int = 0,
-    val moodPoints: List<MoodPoint> = moodPointDefault,
-    val perfectDays: Int = 0
+    val summaryUiState: ReportSummaryUiState = ReportSummaryUiState(),
+    val completedUiState: ReportCompletedUiState = ReportCompletedUiState(),
+    val rateLineUiState: ReportRateLineUiState = ReportRateLineUiState(),
+    val rateCalendarUiState: ReportRateCalendarUiState = ReportRateCalendarUiState(),
+    val moodPoints: List<MoodPoint> = moodPointDefault
 )
