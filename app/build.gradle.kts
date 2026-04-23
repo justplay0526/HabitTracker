0f61ecb5ec91fa209f97dc77a951ea11f673d36b
justplay0526 <justplay0526@gmail.com>
2026-04-23 14:31:59 +0800

    chore: chore v1.1c2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.justplay.habittracker"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
